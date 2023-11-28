1679e732cafbd91ed0667e07487ca32d45322620
4itech-paul <paul@4idps.com>
2023-11-28 15:39:31 +0800

    feat: configure postgresql integration with TypeOrm in the app module
    - add TypeOrm async configuration to root module for postgres database
    - update package.json to include TypeOrm and PostgreSQL driver dependencies
    - set up database connection settings in .env and load through ConfigService
    - enable automatic entity loading and synchronization in TypeOrm module setup
    - ensure DB logging setting is configurable through environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        schema: configService.get('DB_SCHEMA'),
+        autoLoadEntities: true, // every entity registered through the forFeature() method will be automatically added to the entities array of the configuration object.
+        synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+        logging: !!configService.get('DB_LOGGING'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
