847675c8a2b88514235b115d2f9ac51795b7a601
skyran1278 <skyran1278@gmail.com>
2023-12-30 22:57:16 +0800

    feat(query): add utility method to where input types for Domain1 and User
    - introduce a method to convert where input types to `FindOptionsWhere`
    - enable nullable return type for better compatibility with TypeORM
    - enhance querying capabilities in Domain1 and User modules

--- src/domain1/query/domain1-where.input.ts
+++ src/domain1/query/domain1-where.input.ts
@@ -1,7 +1,14 @@
 import { InputType } from '@nestjs/graphql';
+import { Nullable } from 'src/common/base.service';
 import { PartialAndOmitType } from 'src/common/partial-and-omit-type';
+import { FindOptionsWhere } from 'typeorm';
 
 import { Domain1 } from '../domain1.entity';
 
 @InputType()
-export class Domain1WhereInput extends PartialAndOmitType(Domain1, []) {}
+export class Domain1WhereInput extends PartialAndOmitType(Domain1, []) {
+  toFindOptionsWhere(): Nullable<FindOptionsWhere<Domain1>> | undefined {
+    const { ...where } = this;
+    return { ...where };
+  }
+}
--- src/user/query/user-where.input.ts
+++ src/user/query/user-where.input.ts
@@ -1,7 +1,14 @@
 import { InputType } from '@nestjs/graphql';
+import { Nullable } from 'src/common/base.service';
 import { PartialAndOmitType } from 'src/common/partial-and-omit-type';
+import { FindOptionsWhere } from 'typeorm';
 
 import { User } from '../user.entity';
 
 @InputType()
-export class UserWhereInput extends PartialAndOmitType(User, []) {}
+export class UserWhereInput extends PartialAndOmitType(User, []) {
+  toFindOptionsWhere(): Nullable<FindOptionsWhere<User>> | undefined {
+    const { ...where } = this;
+    return { ...where };
+  }
+}
