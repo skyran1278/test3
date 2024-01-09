4439d8bea0dc9e3c007dccf4030ea19f73c3f379
skyran1278 <skyran1278@gmail.com>
2024-01-09 08:46:56 +0800

    refactor(entities): streamline decorators for entity fields
    - introduced custom `FieldColumn` decorator to combine GraphQL and TypeORM definitions
    - removed redundant imports and decorators across entity files
    - enhanced readability and maintainability of the entity definitions

--- src/domain1/domain1.entity.ts
+++ src/domain1/domain1.entity.ts
@@ -1,12 +1,12 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
 import { Maybe } from 'graphql/jsutils/Maybe';
+import { FieldColumn } from 'src/common/field-column.decorator';
 import { MetaEntity } from 'src/common/meta.entity';
-import { Column, Entity } from 'typeorm';
+import { Entity } from 'typeorm';
 
 @Entity()
 @ObjectType({ implements: MetaEntity })
 export class Domain1 extends MetaEntity {
-  @Field(() => Int, { description: 'domain1001', nullable: true })
-  @Column({ type: 'int', nullable: true, comment: 'domain1001' })
+  @FieldColumn({ type: 'int', nullable: true, comment: 'domain1001' })
   domain1001?: Maybe<number>;
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,15 +1,14 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
+import { FieldColumn } from 'src/common/field-column.decorator';
 import { MetaEntity } from 'src/common/meta.entity';
-import { Column, Entity } from 'typeorm';
+import { Entity } from 'typeorm';
 
 @Entity()
 @ObjectType({ implements: MetaEntity })
 export class User extends MetaEntity {
-  @Field(() => Int, { description: 'user001' })
-  @Column({ type: 'int', comment: 'user001' })
+  @FieldColumn({ type: 'int', comment: 'user001' })
   user001!: number;
 
-  @Field(() => Int, { description: 'user002' })
-  @Column({ type: 'int', comment: 'user002' })
+  @FieldColumn({ type: 'int', comment: 'user002' })
   user002!: number;
 }
