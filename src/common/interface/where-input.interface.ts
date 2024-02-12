c8e2b1a9d06256fdf14cb3bb95f01da91384df40
skyran1278 <skyran1278@gmail.com>
2024-02-13 00:31:08 +0800

    refactor(common): rename IWhereInput to WhereInput for consistency

--- src/common/interface/where-input.interface.ts
+++ src/common/interface/where-input.interface.ts
@@ -3,6 +3,6 @@ import { FindOptionsWhere } from 'typeorm';
 import { MetaEntity } from '../meta.entity';
 import { Nullable } from './nullable.interface';
 
-export interface IWhereInput<T extends MetaEntity> {
+export interface WhereInput<T extends MetaEntity> {
   toFindOptionsWhere(): Nullable<FindOptionsWhere<T>> | undefined;
 }
