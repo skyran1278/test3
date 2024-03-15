a9145acf4cbb47e5f25e79483e4d969ced4a2bde
skyran1278 <skyran1278@gmail.com>
2024-03-15 15:07:23 +0800

    feat(domain-1): add DataLoader and GraphQL resolver for Domain1 ID queries
    - implement Domain1IdLoader to optimize batch loading of Domain1 entities
    - introduce Domain1IdResolver to resolve Domain1 entities in GraphQL queries
    - create Domain1Id GraphQL interface type to facilitate ID based querying

---
+++ src/domain-1/domain-1-id.loader.ts
@@ -0,0 +1,26 @@
+// user-loader.service.ts
+import { Injectable, Scope } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import DataLoader from 'dataloader';
+import { Maybe } from 'graphql/jsutils/Maybe';
+import { In, Repository } from 'typeorm';
+
+import { Domain1 } from './domain-1.entity';
+
+@Injectable({ scope: Scope.REQUEST })
+export class Domain1IdLoader extends DataLoader<string, Maybe<Domain1>> {
+  constructor(
+    @InjectRepository(Domain1)
+    private readonly repo: Repository<Domain1>,
+  ) {
+    super(async (keys: readonly string[]): Promise<Maybe<Domain1>[]> => {
+      const daoArray = await this.repo.find({
+        where: {
+          id: In(keys),
+        },
+      });
+      const daoMap = new Map(daoArray.map((dao) => [dao.id, dao]));
+      return keys.map((key) => daoMap.get(key));
+    });
+  }
+}
