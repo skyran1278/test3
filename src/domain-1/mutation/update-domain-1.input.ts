953ec6c9bb091543c06546480e8681eac336df4e
skyran1278 <skyran1278@gmail.com>
2024-03-31 00:41:01 +0800

    refactor(domain-1): streamline UpdateDomain1Input inheritance structure
    - updated UpdateDomain1Input to inherit from OmitType with an empty omission array
    - maintains previous functionality while setting the stage for potential future exclusions in the
    DTO structure

--- src/domain-1/mutation/update-domain-1.input.ts
+++ src/domain-1/mutation/update-domain-1.input.ts
@@ -1,9 +1,12 @@
-import { Field, ID, InputType, PartialType } from '@nestjs/graphql';
+import { Field, ID, InputType, OmitType, PartialType } from '@nestjs/graphql';
 
 import { CreateDomain1Input } from './create-domain-1.input';
 
 @InputType()
-export class UpdateDomain1Input extends PartialType(CreateDomain1Input) {
+export class UpdateDomain1Input extends OmitType(
+  PartialType(CreateDomain1Input),
+  [],
+) {
   @Field(() => ID)
   id!: string;
 }
