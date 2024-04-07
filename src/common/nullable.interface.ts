f664e2c52765789118079de92e6b7da86bb87a3f
skyran1278 <skyran1278@gmail.com>
2024-04-07 14:27:42 +0800

    style(common): clean up formatting in NullableProperty type definition
    - remove extra indents to enhance code readability
    - maintain code consistency across type definitions

--- src/common/nullable.interface.ts
+++ src/common/nullable.interface.ts
@@ -1,9 +1,7 @@
-export type NullableProperty<Property> = Property extends Record<
-  string,
-  unknown
->
-  ? Nullable<Property>
-  : Property | null;
+export type NullableProperty<Property> =
+  Property extends Record<string, unknown>
+    ? Nullable<Property>
+    : Property | null;
 
 export type Nullable<T> = {
   [P in keyof T]?: NullableProperty<T[P]>;
