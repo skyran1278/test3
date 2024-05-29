eb75ebc24292bab53cf2cda1ec9c427e383afaf7
skyran1278 <skyran1278@gmail.com>
2024-05-29 21:49:41 +0800

    test(test): enhance e2e tests with health check endpoint
    - ensure transactional context is initialized for tests
    - add test for `/health` endpoint to validate app health indicators
    - switch from `beforeEach` to `beforeAll` for setup consistency
    - clean up resources properly by using `afterAll`

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,19 +1,19 @@
-import 'reflect-metadata';
-
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
+import {
+  StorageDriver,
+  initializeTransactionalContext,
+} from 'typeorm-transactional';
 
 import { AppModule } from './../src/app.module';
-
-jest.mock('typeorm-transactional', () => ({
-  Transactional: () => () => ({}),
-}));
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -23,9 +23,76 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/health (GET)', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return request(app.getHttpServer())
+      .get('/health')
+      .expect(200)
+      .expect({
+        status: 'ok',
+        info: {
+          database: { status: 'up' },
+          memory_heap: { status: 'up' },
+          memory_rss: { status: 'up' },
+          storage: { status: 'up' },
+        },
+        error: {},
+        details: {
+          database: { status: 'up' },
+          memory_heap: { status: 'up' },
+          memory_rss: { status: 'up' },
+          storage: { status: 'up' },
+        },
+      });
+  });
+
+  // it('/graphql (POST)', () => {
+  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  //   return request(app.getHttpServer())
+  //     .post('/graphql')
+  //     .send({
+  //       query:
+  //         'mutation CreateDomain0001($input: CreateDomain0001Input!) {\n  createDomain0001(input: $input) {\n    domain0001 {\n      createdUser {\n        id\n      }\n      createdUserId\n      createdAt\n      domain0001001\n      id\n      updatedUser {\n        id\n      }\n      updatedUserId\n      updatedAt\n    }\n  }\n}',
+  //       variables: {
+  //         input: {
+  //           domain0001001: 1,
+  //         },
+  //       },
+  //       operationName: 'CreateDomain0001',
+  //     })
+  //     .expect(200)
+  //     .expect((res) => {
+  //       expect(res.body).toEqual({
+  //         data: {
+  //           createDomain0001: {
+  //             domain0001: {
+  //               createdUser: {
+  //                 id: '94107a27-4c24-4912-be7b-6f4b0b462acb',
+  //               },
+  //               createdUserId: '94107a27-4c24-4912-be7b-6f4b0b462acb',
+  //               createdAt: '2024-04-24T05:25:51.634Z',
+  //               domain0001001: 1,
+  //               id: '51e7101b-162c-4326-8388-e839101dd15f',
+  //               updatedUser: {
+  //                 id: '94107a27-4c24-4912-be7b-6f4b0b462acb',
+  //               },
+  //               updatedUserId: '94107a27-4c24-4912-be7b-6f4b0b462acb',
+  //               updatedAt: '2024-04-24T05:25:51.634Z',
+  //             },
+  //           },
+  //         },
+  //       });
+  //     });
+  // });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
