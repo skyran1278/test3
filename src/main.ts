e868f3b78c15993afbfe91af863da46cf38e0e53
Nai You-Ran <skyran1278@gmail.com>
2024-05-29 09:24:17 +0800

    feat(#2): health module
    * build(dependencies): add @nestjs/terminus for health checks
    
    - introduce @nestjs/terminus to facilitate application health checks
    - enhance system monitoring and reliability
    
    * feat(health): add health check endpoint
    
    - introduce health check endpoint using @nestjs/terminus for monitoring
    - include checks for database connectivity, memory usage, and disk storage
    - enhance observability and reliability of the application through health checks
    
    * feat(src): enable shutdown hooks for NestJS app
    
    - ensure graceful shutdown by enabling shutdown hooks
    - add context regarding multiple instances in a single Node process to avoid memory issues

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,14 @@ async function bootstrap() {
     // bufferLogs: true,
   });
   // app.useLogger(app.get(WinstonLogger));
+
+  // https://docs.nestjs.com/fundamentals/lifecycle-events#application-shutdown
+  // enableShutdownHooks consumes memory by starting listeners.
+  // In cases where you are running multiple Nest apps in a single Node process (e.g., when running parallel tests with Jest), Node may complain about excessive listener processes.
+  // For this reason, enableShutdownHooks is not enabled by default.
+  // Be aware of this condition when you are running multiple instances in a single Node process.
+  app.enableShutdownHooks();
+
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(
