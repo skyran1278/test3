1132cb3622e0eb4e3f3685a9b393badbf1f39ec0
skyran1278 <skyran1278@gmail.com>
2024-05-29 21:52:08 +0800

    test(domain-0008): mock dependencies in resolver and service specs
    - replace actual services with empty mocks for clarity and isolation
    - shorten service instance retrieval statement in both specs

--- src/domain-0008/domain-0008.service.spec.ts
+++ src/domain-0008/domain-0008.service.spec.ts
@@ -8,11 +8,11 @@ describe('Domain0008Service', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [Domain0008Service],
-    }).compile();
+    })
+      .useMocker(() => ({}))
+      .compile();
 
-    service = module.get<Domain0008Service>(
-      Domain0008Service,
-    );
+    service = module.get<Domain0008Service>(Domain0008Service);
   });
 
   it('should be defined', () => {
