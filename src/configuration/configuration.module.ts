4c570d4631ad3808ae69ab2d0baf76a0e2b11507
skyran1278 <skyran1278@gmail.com>
2024-05-30 20:20:01 +0800

    refactor(configuration): remove commented-out validation schema
    - simplify configuration module by eliminating commented-out Joi validation schema
    - retain only necessary environment variable settings and validations
    - improve code readability and maintenance

--- src/configuration/configuration.module.ts
+++ src/configuration/configuration.module.ts
@@ -14,46 +14,6 @@ import { validate } from './configuration.validation';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
       validate,
-      // validationSchema: Joi.object({
-      //   NODE_ENV: Joi.string()
-      //     .valid('development', 'production', 'test', 'provision')
-      //     .default('development'),
-      //   PORT: Joi.number().port().default(3010),
-      //   NEST_DEBUG: Joi.boolean().default(false),
-
-      //   // Database
-      //   DB_HOST: Joi.string().default('localhost'),
-      //   DB_PORT: Joi.number().port().default(5510),
-      //   DB_USERNAME: Joi.string().default('postgres'),
-      //   DB_PASSWORD: Joi.string().default('root'),
-      //   DB_NAME: Joi.string().default('postgres'),
-      //   DB_SCHEMA: Joi.string().default('public'),
-      //   DB_LOGGING: Joi.boolean().default(true),
-
-      //   // GraphQL
-      //   GRAPHQL_SERVER: Joi.string()
-      //     .valid('production', 'development')
-      //     .default('development'),
-
-      //   // Redis
-      //   REDIS_HOST: Joi.string().default('localhost'),
-      //   REDIS_PORT: Joi.number().port().default(6379),
-
-      //   // JWT
-      //   JWT_SECRET: Joi.string().default('secret'),
-      //   JWT_EXPIRES_IN: Joi.string().default('100d'),
-
-      //   // GitHub
-      //   GITHUB_TOKEN: Joi.string().default(''),
-
-      //   // Test
-      //   TEST_TOKEN: Joi.string().default(''),
-      // }),
-      // validationOptions: {
-      //   allowUnknown: false,
-      //   abortEarly: false,
-      //   debug: true,
-      // },
     }),
   ],
 })
