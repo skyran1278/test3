3519e9f5c435cdd8f0c670568ef40c757d3ea47e
skyran1278 <skyran1278@gmail.com>
2024-05-30 16:07:36 +0800

    refactor(error): extract constructor options into its own interface
    - improve readability by defining a `CustomErrorOptions` interface
    - maintain documentation consistency within the `CustomError` class
    - ensure cleaner handling and passing of parameters to the constructor

--- src/error/custom.error.ts
+++ src/error/custom.error.ts
@@ -17,29 +17,31 @@ export class ErrorInfo {
   }
 }
 
+/**
+ * @see https://github.com/nestjs/nest/blob/aa7538ffbe8608c41ece2a035b872e5032f57763/packages/common/exceptions/bad-request.exception.ts#L11
+ * @see https://github.com/nestjs/nest/blob/aa7538ffbe8608c41ece2a035b872e5032f57763/packages/common/exceptions/http.exception.ts#L26
+ * @param message Warning: Error messages are not part of the API surface. They are subject to changes without notice. Application code must not have a hard dependency on error messages.
+ * @param httpErrorCode Individual APIs must avoid defining additional error codes, since developers are very unlikely to write logic to handle a large number of error codes. For reference, handling an average of three error codes per API call would mean most application logic would just be for error handling, which would not be a good developer experience.
+ * @param reason The reason field is intended to be used by the API to communicate the high-level cause of the error. It is intended to be used by the API consumer to understand the error and act accordingly.
+ * @param details Additional information about the error.
+ * @param cause This helps in maintaining a chain of errors, providing more context about the error's origin.
+ */
+interface CustomErrorOptions {
+  message: string;
+  httpErrorCode: HttpStatus;
+  reason?: ErrorReasonEnum;
+  detail?: unknown;
+  cause?: Error;
+}
+
 export class CustomError extends HttpException {
-  /**
-   * @see https://github.com/nestjs/nest/blob/aa7538ffbe8608c41ece2a035b872e5032f57763/packages/common/exceptions/bad-request.exception.ts#L11
-   * @see https://github.com/nestjs/nest/blob/aa7538ffbe8608c41ece2a035b872e5032f57763/packages/common/exceptions/http.exception.ts#L26
-   * @param message Warning: Error messages are not part of the API surface. They are subject to changes without notice. Application code must not have a hard dependency on error messages.
-   * @param httpErrorCode Individual APIs must avoid defining additional error codes, since developers are very unlikely to write logic to handle a large number of error codes. For reference, handling an average of three error codes per API call would mean most application logic would just be for error handling, which would not be a good developer experience.
-   * @param reason The reason field is intended to be used by the API to communicate the high-level cause of the error. It is intended to be used by the API consumer to understand the error and act accordingly.
-   * @param details Additional information about the error.
-   * @param cause This helps in maintaining a chain of errors, providing more context about the error's origin.
-   */
   constructor({
     message,
     httpErrorCode,
     reason,
     detail,
     cause,
-  }: {
-    message: string;
-    httpErrorCode: HttpStatus;
-    reason?: ErrorReasonEnum;
-    detail?: unknown;
-    cause?: Error;
-  }) {
+  }: CustomErrorOptions) {
     const response: CustomHttpExceptionBody = {
       message,
       statusCode: httpErrorCode,
