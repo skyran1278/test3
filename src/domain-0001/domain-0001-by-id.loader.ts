60e5808c8e63c9531ad1c5658157aac36c1ea154
skyran1278 <skyran1278@gmail.com>
2024-06-22 13:49:58 +0800

    refactor(domain-0001): update data loader to use custom repository
    - switch to Domain0001Repository for better type safety and custom methods
    - remove unused InjectRepository decorator to streamline dependencies

--- src/domain-0001/domain-0001-by-id.loader.ts
+++ src/domain-0001/domain-0001-by-id.loader.ts
@@ -1,20 +1,17 @@
 import { Injectable, Scope } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import DataLoader from 'dataloader';
 import { Maybe } from 'graphql/jsutils/Maybe';
-import { In, Repository } from 'typeorm';
+import { In } from 'typeorm';
 
 import { Domain0001 } from './domain-0001.entity';
+import { Domain0001Repository } from './domain-0001.repository';
 
 @Injectable({ scope: Scope.REQUEST })
 export class Domain0001ByIdLoader extends DataLoader<
   string,
   Maybe<Domain0001>
 > {
-  constructor(
-    @InjectRepository(Domain0001)
-    private readonly repo: Repository<Domain0001>,
-  ) {
+  constructor(private readonly repo: Domain0001Repository) {
     super(async (keys: readonly string[]): Promise<Maybe<Domain0001>[]> => {
       const daoArray = await this.repo.find({
         where: {
