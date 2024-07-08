8e11b1ecf572a1164a0b817c999fd881d870264a
skyran1278 <skyran1278@gmail.com>
2024-07-08 17:51:25 +0800

    refactor(cdk): modularize VPC creation into a separate construct
    - create a dedicated `Vpc` construct to handle VPC creation
    - add VPC-related configurations, including subnet setup and flow logs
    - integrate the new `Vpc` construct into the main stack to streamline code structure

---
+++ cdk/lib/vpc.ts
@@ -0,0 +1,43 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
+
+// interface VpcProps extends cdk.StackProps {}
+
+export class Vpc extends Construct {
+  public readonly vpc: ec2.IVpc;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.vpc = new ec2.Vpc(this, 'Test3Vpc', {
+      // Disable NAT gateways, free tier does not include them
+      natGateways: 0,
+
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        // {
+        //   cidrMask: 24,
+        //   name: 'Private',
+        //   subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        // },
+      ],
+    });
+
+    // Create a CloudWatch Log Group for Flow Logs
+    const logGroup = new LogGroup(this, 'LogGroup', {
+      retention: RetentionDays.ONE_WEEK,
+    });
+
+    // Create a Flow Log for the VPC
+    new ec2.FlowLog(this, 'FlowLog', {
+      resourceType: ec2.FlowLogResourceType.fromVpc(this.vpc),
+      destination: ec2.FlowLogDestination.toCloudWatchLogs(logGroup),
+    });
+  }
+}
