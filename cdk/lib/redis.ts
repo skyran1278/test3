b4b09ac345bb6f39f4b901a18a4fb570a285cd3d
skyran1278 <skyran1278@gmail.com>
2024-07-14 15:51:11 +0800

    feat(cdk): add custom parameter group to Redis cluster
    - configure noeviction memory policy in Redis to prevent data loss
    - improve cache stability by using a custom parameter group

--- cdk/lib/redis.ts
+++ cdk/lib/redis.ts
@@ -27,6 +27,18 @@ export class Redis extends Construct {
       'Allow Redis inbound',
     );
 
+    const parameterGroup = new elasticache.CfnParameterGroup(
+      this,
+      'ParameterGroup',
+      {
+        cacheParameterGroupFamily: 'redis7',
+        description: 'Custom parameter group for Redis with noeviction policy',
+        properties: {
+          'maxmemory-policy': 'noeviction',
+        },
+      },
+    );
+
     // Create the Redis subnet group
     const subnetGroup = new elasticache.CfnSubnetGroup(this, 'SubnetGroup', {
       description: 'Subnet group for Redis cluster',
@@ -34,12 +46,14 @@ export class Redis extends Construct {
     });
 
     const redisCluster = new elasticache.CfnCacheCluster(this, 'Cluster', {
-      // Free tier eligible instance type
+      // https://aws.amazon.com/tw/elasticache/pricing/
+      // 750 hours of ElastiCache cache.t2.micro or cache.t3.micro node usage for free for up to 12 months.
       cacheNodeType: 'cache.t3.micro',
       engine: 'redis',
       numCacheNodes: 1,
       vpcSecurityGroupIds: [securityGroup.securityGroupId],
       cacheSubnetGroupName: subnetGroup.ref,
+      cacheParameterGroupName: parameterGroup.ref,
     });
 
     // Establishes the dependency between cache and subnetGroup, so that they can be deleted in the right order
