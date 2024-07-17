6e8e41177745f50b91857996b2441c03760f53a7
skyran1278 <skyran1278@gmail.com>
2024-07-17 10:27:52 +0800

    feat(cdk): enhance Redis security and customization
    - expose securityGroup as a class property for better access control
    - obfuscate Redis endpoint port to 7982 for enhanced security
    - dynamically assign Redis endpoint port in service configuration

--- cdk/lib/redis.ts
+++ cdk/lib/redis.ts
@@ -13,19 +13,20 @@ interface RedisProps extends StackProps {
 
 export class Redis extends Construct {
   public readonly cluster: CfnCacheCluster;
+  public readonly securityGroup: SecurityGroup;
 
   constructor(scope: Construct, id: string, props: RedisProps) {
     super(scope, id);
 
     // Create a security group for the Redis cluster
-    const securityGroup = new SecurityGroup(this, 'SecurityGroup', {
+    this.securityGroup = new SecurityGroup(this, 'SecurityGroup', {
       vpc: props.vpc,
       description: 'Allow Redis access',
       allowAllOutbound: true,
     });
 
     // Allow inbound traffic on port 6379 (default Redis port)
-    securityGroup.addIngressRule(
+    this.securityGroup.addIngressRule(
       Peer.anyIpv4(),
       Port.tcp(6379),
       'Allow Redis inbound',
@@ -51,9 +52,14 @@ export class Redis extends Construct {
       cacheNodeType: 'cache.t3.micro',
       engine: 'redis',
       numCacheNodes: 1,
-      vpcSecurityGroupIds: [securityGroup.securityGroupId],
+      vpcSecurityGroupIds: [this.securityGroup.securityGroupId],
       cacheSubnetGroupName: subnetGroup.ref,
       cacheParameterGroupName: parameterGroup.ref,
+
+      // AwsSolutions-AEC5
+      // The ElastiCache cluster uses the default endpoint port.
+      // Port obfuscation (using a non default endpoint port) adds an additional layer of defense against non-targeted attacks (i.e. Redis port 6379 and Memcached port 11211).
+      port: 7982,
     });
 
     // Establishes the dependency between cache and subnetGroup, so that they can be deleted in the right order
