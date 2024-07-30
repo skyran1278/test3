c7beb78ee6e233f08a2e6bbfd2caa86438874590
skyran1278 <skyran1278@gmail.com>
2024-07-30 14:35:09 +0800

    refactor(eslint): update parserOptions project value to true because of cdk
    - Update the parserOptions project value in the .eslintrc.js file to true
    - This change allows ESLint to automatically detect the project configuration file
    - https://typescript-eslint.io/packages/parser/#project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: true,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
