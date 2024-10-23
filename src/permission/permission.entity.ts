9a5baa7f580ce105b4ab4aef7dcc09ee56f8bc0f
skyran1278 <skyran1278@gmail.com>
2024-10-23 21:33:07 +0800

    fix(permission): correct type for conditions field
    - replace Record<string, unknown> with ObjectLiteral for type safety
    - resolve TypeScript error when inserting records

--- src/permission/permission.entity.ts
+++ src/permission/permission.entity.ts
@@ -1,7 +1,7 @@
 import { RawRule } from '@casl/ability';
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Maybe } from 'graphql/jsutils/Maybe';
-import { Entity, ManyToMany } from 'typeorm';
+import { Entity, ManyToMany, ObjectLiteral } from 'typeorm';
 
 import { ColumnField } from '../common/column-field.decorator';
 import { MetaEntity } from '../common/meta.entity';
@@ -17,8 +17,9 @@ export class Permission extends MetaEntity implements RawRule {
   @ColumnField({ type: 'varchar', length: 50 })
   subject!: string;
 
+  // fix Type 'Record<string, unknown>' is not assignable to type '(() => string) | _QueryDeepPartialEntity<Maybe<Record<string, unknown>>>' while calling insert
   @ColumnField({ type: 'jsonb', nullable: true })
-  conditions?: Maybe<Record<string, unknown>>;
+  conditions?: Maybe<ObjectLiteral>;
 
   @Field(() => [Role], { nullable: true, description: '角色' })
   @ManyToMany(() => Role, (role) => role.permissions)
