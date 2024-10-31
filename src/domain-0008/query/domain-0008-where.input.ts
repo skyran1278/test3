96ff000432aaf7dcb929ae4cf2008032e03303b4
skyran1278 <skyran1278@gmail.com>
2024-10-31 11:46:56 +0800

    feat(domain-0008): add domain0009 filter to Domain0008WhereInput
    - include domain0009s as a nullable input field
    - update toFindOptionsWhere to handle the new domain0009 filter
    - enhance query capabilities by linking with Domain0009 input type

--- src/domain-0008/query/domain-0008-where.input.ts
+++ src/domain-0008/query/domain-0008-where.input.ts
@@ -3,6 +3,8 @@ import { FindOptionsWhere } from 'typeorm';
 
 import { DeepNullable } from '../../common/nullable.interface';
 import { ToWhereInputType } from '../../common/to-where-input-type';
+import { TypeField } from '../../common/type-field.decorator';
+import { Domain0009WhereInput } from '../../domain-0009/query/domain-0009-where.input';
 import { Domain0008 } from '../domain-0008.entity';
 
 @InputType()
@@ -10,8 +12,11 @@ export class Domain0008WhereInput extends OmitType(
   ToWhereInputType(Domain0008),
   [],
 ) {
+  @TypeField(() => Domain0009WhereInput, { nullable: true })
+  domain0009s?: Domain0009WhereInput;
+
   toFindOptionsWhere(): DeepNullable<FindOptionsWhere<Domain0008>> {
-    const { ...where } = this;
-    return { ...where };
+    const { domain0009s, ...where } = this;
+    return { ...where, domain0009s: domain0009s?.toFindOptionsWhere() };
   }
 }
