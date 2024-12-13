e6927a9a1c71408454ab3cc0dc574af708895b4f
skyran1278 <skyran1278@gmail.com>
2024-12-13 09:31:37 +0800

    refactor(domain-0001): update toFindOptionsWhere return type
    - remove DeepNullable type from toFindOptionsWhere return type
    - ensure compatibility with FindOptionsWhere type from typeorm

--- src/domain-0001/query/domain-0001-where.input.ts
+++ src/domain-0001/query/domain-0001-where.input.ts
@@ -1,7 +1,6 @@
 import { InputType, OmitType } from '@nestjs/graphql';
 import { FindOptionsWhere } from 'typeorm';
 
-import { DeepNullable } from '../../common/nullable.interface';
 import { ToWhereInputType } from '../../common/to-where-input-type';
 import { Domain0001 } from '../domain-0001.entity';
 
@@ -10,7 +9,7 @@ export class Domain0001WhereInput extends OmitType(
   ToWhereInputType(Domain0001),
   [],
 ) {
-  toFindOptionsWhere(): DeepNullable<FindOptionsWhere<Domain0001>> {
+  toFindOptionsWhere(): FindOptionsWhere<Domain0001> {
     const { ...where } = this;
     return { ...where };
   }
